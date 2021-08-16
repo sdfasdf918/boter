57df5e7aad2ce08a74cc3f1ecee72d65c3b439ef
sdfasdf918 <73057754+sdfasdf918@users.noreply.github.com>
2021-08-16 19:43:34 +0200

    Update icon.js

--- commands/public/icon.js
+++ commands/public/icon.js
@@ -1,13 +1,25 @@
 const { Client, Message, MessageEmbed } = require('discord.js');
 
 module.exports = {
-    name: '',
+    name: 'icon',
     /** 
      * @param {Client} client 
      * @param {Message} message 
      * @param {String[]} args 
      */
     run: async(client, message, args) => {
-
+        const bannerUrl = await getUserBannerUrl(message.author.id, { size: 4096 });
+        if (bannerUrl) {
+            const embed = new MessageEmbed()
+                .setTitle(`${message.author.username}'s banner`)
+                .setDescription("Look at my banner, how cool is that?")
+                .setImage(bannerUrl);
+            message.channel.send(embed);
+        } else {
+            message.channel.send(
+                new MessageEmbed()
+                .setDescription("```**You don't have money to buy Discord Nitro... How sad...**```")
+                );
+        }
     }
 }
